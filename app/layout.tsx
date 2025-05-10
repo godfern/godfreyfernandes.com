11c80da32725910c7de1a76029d2532a2eda6d34
Godfrey Fernandes <godfrey.f@BLRETV-7YN0R4RR.local>
2025-05-11 00:13:54 +0530

    Added fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,15 +2,6 @@
 import '../styles/globals.css';
 import type { Metadata } from 'next';
 import { ReactNode } from 'react';
-import { Pacifico } from 'next/font/google';
-
-const pacifico = Pacifico({
-  weight: '400',
-  subsets: ['latin'],
-  display: 'swap',
-  preload: true,
-  fallback: ['system-ui', 'arial']
-});
 
 export const metadata: Metadata = {
   title: 'Godfrey Portfolio',
@@ -19,7 +10,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en" className={pacifico.className}>
+    <html lang="en">
       <body>{children}</body>
     </html>
   );
