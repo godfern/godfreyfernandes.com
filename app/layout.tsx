2065384bf4dabbbaca9464996a579b119fb4139a
Godfrey F <godfern13@gmail.com>
2025-10-04 17:38:12 +0530

    Updated description meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,8 @@ import type { Metadata } from 'next';
 import { ReactNode } from 'react';
 
 export const metadata: Metadata = {
-  title: 'Godfrey Portfolio',
-  description: 'Frontend Developer Portfolio',
+  title: 'Godfrey Fernandes',
+  description: 'Godfrey Fernandes - Frontend Developer',
   appleWebApp: {
     title: 'godfern',
   },
