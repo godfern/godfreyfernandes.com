4c273712e0fc1123ab4f01881e31b1c528556e9d
Godfrey F <godfern13@gmail.com>
2025-10-04 17:34:24 +0530

    Added favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,11 +6,18 @@ import { ReactNode } from 'react';
 export const metadata: Metadata = {
   title: 'Godfrey Portfolio',
   description: 'Frontend Developer Portfolio',
+  appleWebApp: {
+    title: 'godfern',
+  },
+  manifest: '/manifest.json',
 };
 
-export default function RootLayout({ children }: { children: ReactNode }) {
+export default function RootLayout({ children }: Readonly<{ children: ReactNode }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="apple-mobile-web-app-title" content="godfern" />
+      </head>
       <body>{children}</body>
     </html>
   );
